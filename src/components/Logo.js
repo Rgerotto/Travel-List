47e4188ec2498b92def69a30953fc855c3f66ef5
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-10-29 17:43:57 +0100

    app done

---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+export default   function Logo() {
+    return (
+    <header className="header">
+    <h1>Welcome to PackWell</h1>
+    <p>🛫  your checklist for a stress-free trip!  📸</p>
+    </header>
+
+    );
+  }
